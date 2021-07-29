38de4c3bbfa2b888a5a85201845b5ff897128287
PolinaStamenova <polina.stamenova@gmail.com>
2021-07-29 10:47:49 +0300

    Refactor sendData function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import sendData from './sednDataAPI.js';
-import getUser from './getDataAPI.js';
-import removeListItems from './removeListItem.js';
-import './style.css';
+import sendData from "./sednDataAPI.js";
+import constructJson from "./constructJson";
+import getUser from "./getDataAPI.js";
+import removeListItems from "./removeListItem.js";
+import "./style.css";
 
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wwkGIA4V1TgaiFdJxS1PX0cI73nUXzQQnx5UI2Pc/scores';
+const url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wwkGIA4V1TgaiFdJxS1PX0cI73nUXzQQnx5UI2Pc/scores";
 
-const submitBtn = document.getElementById('submitBtn');
+const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener('click', (e) => {
+submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const name = document.getElementById('userName').value;
-  const score = document.getElementById('userScore').value;
+  const name = document.getElementById("userName").value;
+  const score = document.getElementById("userScore").value;
   if (name.length > 0 && score.length > 0) {
-    sendData();
-    document.getElementById('userName').value = '';
-    document.getElementById('userScore').value = '';
+    sendData(constructJson(name, score));
+    document.getElementById("userName").value = "";
+    document.getElementById("userScore").value = "";
   }
 });
 
 getUser(url);
 
-const refreshBtn = document.getElementById('refreshBtn');
-refreshBtn.addEventListener('click', () => {
+const refreshBtn = document.getElementById("refreshBtn");
+refreshBtn.addEventListener("click", () => {
   removeListItems();
   getUser(url);
 });
